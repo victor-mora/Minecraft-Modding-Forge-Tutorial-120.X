61e0103b59be2ca86d5285febe4d17a7507d79e2
victor-mora <60822137+victor-mora@users.noreply.github.com>
2024-04-27 11:11:14 -0700

    Add custom tags for metal detector valuable blocks

--- src/main/java/net/vwmora/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/vwmora/tutorialmod/item/custom/MetalDetectorItem.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.vwmora.tutorialmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -60,6 +61,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return  state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return  state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
