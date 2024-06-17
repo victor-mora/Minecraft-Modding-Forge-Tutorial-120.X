8280da951c3ae39f8b8f87f4274747f0daacd147
victor-mora <60822137+victor-mora@users.noreply.github.com>
2024-06-16 19:21:27 -0700

    Add sapphire tools (axe, hoe, pickaxe, shovel, sword)

--- src/main/java/net/vwmora/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/vwmora/tutorialmod/item/ModCreativeModeTabs.java
@@ -36,6 +36,12 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.PINE_CONE.get());
                         output.accept(ModItems.SAPPHIRE_STAFF.get());
 
+                        output.accept(ModItems.SAPPHIRE_SWORD.get());
+                        output.accept(ModItems.SAPPHIRE_PICKAXE.get());
+                        output.accept(ModItems.SAPPHIRE_AXE.get());
+                        output.accept(ModItems.SAPPHIRE_SHOVEL.get());
+                        output.accept(ModItems.SAPPHIRE_HOE.get());
+
                         output.accept(ModBlocks.SAPPHIRE_STAIRS.get());
                         output.accept(ModBlocks.SAPPHIRE_SLAB.get());
                         output.accept(ModBlocks.SAPPHIRE_BUTTON.get());
