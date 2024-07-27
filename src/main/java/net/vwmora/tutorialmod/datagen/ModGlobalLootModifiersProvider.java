1a00ccd42613b0db4f2e72026ca5f85a1e8d30f5
victor-mora <60822137+victor-mora@users.noreply.github.com>
2024-07-27 14:34:14 -0700

    add metal detector to suspicious sand in desert pyramid

--- src/main/java/net/vwmora/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/vwmora/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
@@ -11,6 +11,7 @@ import net.minecraftforge.common.loot.LootTableIdCondition;
 import net.vwmora.tutorialmod.TutorialMod;
 import net.vwmora.tutorialmod.item.ModItems;
 import net.vwmora.tutorialmod.loot.AddItemModifier;
+import net.vwmora.tutorialmod.loot.AddSusSandItemModifier;
 
 public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     public ModGlobalLootModifiersProvider(PackOutput output) {
@@ -28,6 +29,9 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
         add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+        add("metal_detector_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() }, ModItems.METAL_DETECTOR.get()));
 
 
     }
