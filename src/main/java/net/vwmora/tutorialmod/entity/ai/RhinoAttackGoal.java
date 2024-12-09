84683f87ea4e2766315c4d06447fbbb79f796c72
victor-mora <60822137+victor-mora@users.noreply.github.com>
2024-12-09 01:01:40 -0800

    Add rhino entity attack animations

---
+++ src/main/java/net/vwmora/tutorialmod/entity/ai/RhinoAttackGoal.java
@@ -0,0 +1,96 @@
+package net.vwmora.tutorialmod.entity.ai;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.vwmora.tutorialmod.entity.custom.RhinoEntity;
+
+public class RhinoAttackGoal extends MeleeAttackGoal {
+    private final RhinoEntity entity;
+    private int attackDelay = 40;
+    private int ticksUntilNextAttack = 40;
+    private boolean shouldCountTillNextAttack = false;
+    private int ATTACK_RANGE = 5;
+
+    public RhinoAttackGoal(PathfinderMob pMob, double pSpeedModifier, boolean pFollowingTargetEvenIfNotSeen) {
+        super(pMob, pSpeedModifier, pFollowingTargetEvenIfNotSeen);
+        entity = ((RhinoEntity) pMob);
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        attackDelay = 40;
+        ticksUntilNextAttack = 40;
+    }
+
+    @Override
+    protected void checkAndPerformAttack(LivingEntity pEnemy) {
+        if (isEnemyWithinAttackDistance(pEnemy)) {
+            shouldCountTillNextAttack = true;
+
+            if(isTimeToStartAttackAnimation()) {
+                entity.setAttacking(true);
+            }
+
+            if(isTimeToAttack()) {
+                this.mob.getLookControl().setLookAt(pEnemy.getX(), pEnemy.getEyeY(), pEnemy.getZ());
+                performAttack(pEnemy);
+            }
+        } else {
+            resetAttackCooldown();
+            shouldCountTillNextAttack = false;
+            entity.setAttacking(false);
+            entity.attackAnimationTimeout = 0;
+        }
+    }
+
+    private boolean isEnemyWithinAttackDistance(LivingEntity pEnemy) {
+//        return pDistToEnemySqr <= this.getAttackReachSqr(pEnemy);
+            int diffX = Math.abs(pEnemy.getBlockX() - entity.getBlockX());
+            int diffY = Math.abs(pEnemy.getBlockY() - entity.getBlockY());;
+            int diffZ = Math.abs(pEnemy.getBlockZ() - entity.getBlockZ());;
+
+        // Make a private int called attack range (I have mine set to 5 and it works alright)
+            return diffX + diffY + diffZ <= this.ATTACK_RANGE;
+
+    }
+
+    protected void resetAttackCooldown() {
+        this.ticksUntilNextAttack = this.adjustedTickDelay(attackDelay * 2);
+    }
+
+    protected boolean isTimeToAttack() {
+        return this.ticksUntilNextAttack <= 0;
+    }
+
+    protected boolean isTimeToStartAttackAnimation() {
+        return this.ticksUntilNextAttack <= attackDelay;
+    }
+
+    protected int getTicksUntilNextAttack() {
+        return this.ticksUntilNextAttack;
+    }
+
+
+    protected void performAttack(LivingEntity pEnemy) {
+        this.resetAttackCooldown();
+        this.mob.swing(InteractionHand.MAIN_HAND);
+        this.mob.doHurtTarget(pEnemy);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if(shouldCountTillNextAttack) {
+            this.ticksUntilNextAttack = Math.max(this.ticksUntilNextAttack - 1, 0);
+        }
+    }
+
+    @Override
+    public void stop() {
+        entity.setAttacking(false);
+        super.stop();
+    }
+}
--- src/main/java/net/vwmora/tutorialmod/entity/custom/RhinoEntity.java
+++ src/main/java/net/vwmora/tutorialmod/entity/custom/RhinoEntity.java
@@ -1,5 +1,9 @@
 package net.vwmora.tutorialmod.entity.custom;
 
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.vwmora.tutorialmod.entity.ModEntities;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -18,15 +22,21 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.vwmora.tutorialmod.entity.ai.RhinoAttackGoal;
 import org.jetbrains.annotations.Nullable;
 
 public class RhinoEntity extends Animal {
+    private static final EntityDataAccessor<Boolean> ATTACKING =
+            SynchedEntityData.defineId(RhinoEntity.class, EntityDataSerializers.BOOLEAN);
     public RhinoEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+
+    public final AnimationState attackAnimationState = new AnimationState();
+    public int attackAnimationTimeout = 0;
 
 
     @Override
@@ -45,6 +55,16 @@ public class RhinoEntity extends Animal {
         } else {
             --this.idleAnimationTimeout;
         }
+        if(this.isAttacking() && attackAnimationTimeout <= 0) {
+            attackAnimationTimeout = 80; // Length in ticks of your animation
+            attackAnimationState.start(this.tickCount);
+        } else {
+            --this.attackAnimationTimeout;
+        }
+
+        if(!this.isAttacking()) {
+            attackAnimationState.stop();
+        }
     }
 
     @Override
@@ -59,9 +79,25 @@ public class RhinoEntity extends Animal {
         this.walkAnimation.update(f, 0.2f);
     }
 
+    public void setAttacking(boolean attacking) {
+        this.entityData.set(ATTACKING, attacking);
+    }
+
+    public boolean isAttacking() {
+        return this.entityData.get(ATTACKING);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(ATTACKING, false);
+    }
+
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
+
+        this.goalSelector.addGoal(1, new RhinoAttackGoal(this, 1.0D, true));
 
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.15D));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.2D, Ingredient.of(Items.COOKED_BEEF), false));
@@ -71,6 +107,8 @@ public class RhinoEntity extends Animal {
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.1D));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 3f));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
 
     }
 
